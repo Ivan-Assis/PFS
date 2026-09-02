1f0b418d98cb26816b1f855e95732c9c914bcbba
Ivan Assis <ivan.assis766@gmail.com>
2026-09-02 15:53:34 -0300

    wip: curso_next next-01-introdução

---
+++ cursoNextProjetos/my-app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+  return(
+    <div className="text-3x1 text-blue-600 font-bold">
+      Olá Mundo!
+    </div>
+  )
+}
--- next01/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-3xl text-blue-600 font-bold">
-      Olá mundo!
-    </div>
-  );
-}
